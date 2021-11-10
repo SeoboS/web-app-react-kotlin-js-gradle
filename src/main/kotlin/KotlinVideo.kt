bef6687d3b2a6fa6fe155fdc996117f08afb2cde
Seo Bo Shim <7828315+SeoboS@users.noreply.github.com>
2021-11-10 17:10:36 -0500

    static ui code from last session

---
+++ src/main/kotlin/KotlinVideo.kt
@@ -0,0 +1,9 @@
+
+data class KotlinVideo (
+    override val id: Int,
+    override val title: String,
+    override val speaker: String,
+    override val videoUrl: String
+) : Video {
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,65 @@
 import kotlinx.browser.document
+import kotlinx.css.*
+import react.dom.*
+import styled.*
 
 fun main() {
-    document.bgColor = "blue"
+//    document.bgColor = "red"
+    val unwatchedVideos = listOf(
+        KotlinVideo(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+        KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+        KotlinVideo(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+    )
+    val watchedVideos = listOf(
+        KotlinVideo(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+    )
+
+
+    render(document.getElementById("root")){
+        styledH1{
+            css{
+                fontFamily = "sans-serif"
+            }
+            +"KotlinConf Explorer!"
+        }
+        div{
+            h3{
+                +"Videos to Watch"
+            }
+            for (video in unwatchedVideos){
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+            h3{
+                +"Vidoes Watched"
+            }
+            for(video in watchedVideos){
+                p{
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+        }
+        img{
+            attrs{
+                src = "https://www.looper.com/img/gallery/craziest-nicolas-cage-performances/vampires-kiss-1988.jpg"
+            }
+        }
+
+        styledDiv{
+            css{
+                position = Position.absolute
+                top = 10.px
+                right = 10.px
+            }
+            h3{
+                +"John Doe"
+            }
+            img{
+                attrs{
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+    }
 }
